ca84a1df725d5e029a9849e7ce73bac41acbb356
Damien Le Thiec <damien.lethiec@gmail.com>
2017-01-12 03:42:02 +0100

    improvement disposition main image home

--- app/assets/javascripts/footer.js
+++ app/assets/javascripts/footer.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $(document).scroll(function () {
     var position = $(this).scrollTop();
-    if (position > 140) {
+    if (position > 180) {
       $('#footer').css({
         transform: 'translateY(-60px)',
         transition: 'all 0.3s ease'
