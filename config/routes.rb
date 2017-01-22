424fe82c7313f27c0198b2eed37c1556d595c362
Damien Le Thiec <damien.lethiec@gmail.com>
2017-01-22 23:21:42 +0100

    end home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get "pages/how_I_work", :as => 'how_I_work'
   resources :games, only: :new
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
